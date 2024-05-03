8c5a67a6d3598cfc749e905613651a176efe3ea6
alex <alex888me@gmail.com>
2024-05-03 14:26:35 +0200

    Slowo zadanie

--- slowo/__init__.py
+++ slowo/__init__.py
@@ -18,23 +18,68 @@ Poprawna odpowiedzią jest:
 
 Możliwe podziały to: o-cena, o-ce-na, o-cen-a, oc-ena, oc-e-na, ocen-a, oce-na, ocen-a
 """
-from typing import List
+from typing import List, Set
 
 
 class Slowo_1:
-
+    samogloski = {'a', 'ą', 'e', 'ę', 'u', 'i', 'o', 'y'}
 
     def __init__(self, word: str):
-        self.word = word
+        self.word = word.lower()
+        self._check_samogloska()
+        self._find_samogloski()
 
     @property
-    def possibilites(self) -> str:
-        return ''
+    def possibilites(self) -> Set[str]:
+        return {}
 
     @property
     def number_of_divisions(self) -> int:
         return 0
 
-    def _spit_word(self) -> List[str]:
-        self.word
+    def _check_samogloska(self):
+        if not self._has_samogloska(self.word):
+            raise ValueError(f"'{self.word}' nie ma samoglosek.")
 
+    def _has_samogloska(self, word_to_check):
+        set_of_letters = {litera for litera in word_to_check}
+        return bool(set_of_letters.intersection(self.samogloski))
+
+    def _find_samogloski(self):
+        self.indexes = []
+
+        for i, letter in enumerate(self.word):
+            if letter in self.samogloski:
+                self.indexes.append(i)
+
+
+        print(self.indexes)
+        print('------------')
+
+        for i in self.indexes:
+            if i < len(self.word) - 1:
+                print(i, self.word[:i+1], self.word[i+1:])
+            if i > 1:
+                print(i, self.word[:i], self.word[i:])
+
+    # def _split_word(self) -> List[str]:
+    #     # print('_split_word', self.word)
+    #     self.divided_word = []
+    #     self.counter = 0
+    #
+    #     for litera in self.word:
+    #
+    #         if litera in self.samogloski:
+    #             self._append_item(litera)
+    #             self.counter += 1
+    #         else:
+    #             self._append_item(litera)
+    #
+    #     if self._has_samogloska(self.divided_word[-1]):
+    #         print('has samogloska')
+    #     else:
+    #         last_item = self.divided_word.pop(-1)
+    #         # print(last_item)
+    #         self.divided_word[-1] += last_item
+    #     print('divided: ', self.divided_word)
+    #     return [self.word]
--- slowo/test_slowo.py
+++ slowo/test_slowo.py
@@ -1,3 +1,17 @@
+import unittest
 from slowo import Slowo_1
+import slowo
 
 ocena = Slowo_1(word='ocena')
+real = Slowo_1(word='real')
+oabcell = Slowo_1(word='oabcell')
+
+
+class MyTestCase(unittest.TestCase):
+    def test_incorrect_input(self):
+        self.assertRaises(ValueError, slowo.Slowo_1, 'qwrtp')
+        self.assertRaises(ValueError, slowo.Slowo_1, 'ttttt')
+
+
+# ocena.possibilites
+# assert ocena.possibilites == {'o-cena', 'o-ce-na', 'o-cen-a', 'oc-ena', 'oc-e-na', 'ocen-a', 'oce-na', 'ocen-a', 'ocen-a'}
